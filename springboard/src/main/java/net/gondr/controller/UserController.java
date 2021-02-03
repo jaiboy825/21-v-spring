55bac0020ef9288d6ab6e7c5989bd50070afbf08
jaiboy825 <jaiboy@naver.com>
2021-02-03 15:48:25 +0900

    세번째 수업 끝

--- springboard/src/main/java/net/gondr/controller/UserController.java
+++ springboard/src/main/java/net/gondr/controller/UserController.java
@@ -1,14 +1,22 @@
 package net.gondr.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
-import net.gondr.domain.UserVO;
+import net.gondr.domain.RegisterDTO;
+import net.gondr.util.FileUtil;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+
+	@Autowired
+	private ServletContext context;
 
 	@RequestMapping(value = "register", method = RequestMethod.GET)
 	public String viewRegisterPage() {
@@ -16,8 +24,14 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "register", method = RequestMethod.POST)
-	public String registerProcess(UserVO user) {
-		//아직 미구현
+	public String registerProcess(RegisterDTO dto) throws Exception {
+		String uploadPath = context.getRealPath("/WEB-INF/upload");
+		System.out.println(uploadPath);
+
+		MultipartFile file = dto.getProfileImg();
+		String upfile = FileUtil.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
+		System.out.println(uploadPath + "에" + upfile + "로 업로드 되었습니다.");
+
 		return "user/register";
 	}
 }
